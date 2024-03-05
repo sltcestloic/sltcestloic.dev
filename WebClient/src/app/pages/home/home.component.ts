fc9e96b9f17c76579290ae99a78c55050df4bfef
unknown <lbertran@student.42lyon.fr>
2024-03-05 19:50:45 +0100

    feat: disable SSR, add temporary no-mobile view

--- WebClient/src/app/pages/home/home.component.ts
+++ WebClient/src/app/pages/home/home.component.ts
@@ -1,13 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, Host, HostListener, OnInit } from '@angular/core';
 import { ProjectCardComponent } from '../../components/project-card/project-card.component';
+import { NgIf } from '@angular/common';
 
 @Component({
-  selector: 'app-home',
-  standalone: true,
-  imports: [ProjectCardComponent],
-  templateUrl: './home.component.html',
-  styleUrl: './home.component.scss'
+	selector: 'app-home',
+	standalone: true,
+	imports: [ProjectCardComponent, NgIf],
+	templateUrl: './home.component.html',
+	styleUrl: './home.component.scss'
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
 
+	private readonly _mobileWidth = 768;
+
+	public isMobile: boolean;
+
+	ngOnInit(): void {
+		this.isMobile = window.innerWidth < this._mobileWidth;
+	}
+
+	@HostListener('window:resize', ['$event'])
+	onResize(event: any) {
+		this.isMobile = event.target.innerWidth < this._mobileWidth;
+	}
 }
