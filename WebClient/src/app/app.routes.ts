9e9c18973554e878b9f4803a710556bf9bf503a3
unknown <lbertran@student.42lyon.fr>
2024-03-05 18:52:24 +0100

    fix: match any route to home

--- WebClient/src/app/app.routes.ts
+++ WebClient/src/app/app.routes.ts
@@ -2,7 +2,7 @@ import { Routes } from '@angular/router';
 
 export const routes: Routes = [
     {
-        path: '',
+        path: '**',
         loadComponent: () => import('./pages/home/home.component').then(m => m.HomeComponent)
     }
 ];
