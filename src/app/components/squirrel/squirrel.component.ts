ac94631b039e13fb0e5ba40df2fa8e16d134cda4
sltcestloic <root@sltcestloic.dev>
2024-05-08 12:43:15 +0200

    refactor: remove WebClient directory

---
+++ src/app/components/squirrel/squirrel.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'squirrel',
+    standalone: true,
+    imports: [],
+    templateUrl: './squirrel.component.html',
+})
+export class SquirrelComponent {
+
+}
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { SquirrelComponent } from '../../components/squirrel/squirrel.component';
 
 @Component({
     selector: 'app-home',
     standalone: true,
-    imports: [RouterModule],
+    imports: [RouterModule, SquirrelComponent],
     templateUrl: './home.component.html',
     styleUrl: './home.component.scss'
 })
