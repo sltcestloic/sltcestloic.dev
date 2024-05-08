a6698452a447715ec4c5e150aa1fb6ece4e91311
sltcestloic <root@sltcestloic.dev>
2024-05-08 13:49:14 +0200

    feat: add socials & cat pic to fill the empty space

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,11 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { bootstrapTwitterX } from '@ng-icons/bootstrap-icons';
+import { NgIconComponent, provideIcons } from '@ng-icons/core';
+import { octMarkGithub } from '@ng-icons/octicons';
 import { SquirrelComponent } from '../../components/squirrel/squirrel.component';
 
 @Component({
     selector: 'app-home',
     standalone: true,
-    imports: [RouterModule, SquirrelComponent],
+    imports: [RouterModule, SquirrelComponent, NgIconComponent],
+    providers: [provideIcons({ octMarkGithub, bootstrapTwitterX})],
     templateUrl: './home.component.html',
     styleUrl: './home.component.scss'
 })
